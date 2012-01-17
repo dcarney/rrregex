03d33a2fa0efeb5c3665fec073812ebc3a46711f
dcarney <dcarney@gmail.com>
2012-01-17 13:51:41 -0800

    Adds support for regex options. Regex matches and captures are now rendered with Mustache templates.

--- app.rb
+++ app.rb
@@ -2,12 +2,27 @@ require 'sinatra'
 require 'cgi'
 require 'json'
 
-post '/match' do      
-  opts = params.inject({}){ |h,(k,v)| h.merge(k => CGI::unescape(v)) }
+OPTMAP = {
+  'i' => Regexp::IGNORECASE,
+  'x' => Regexp::EXTENDED,
+  'm' => Regexp::MULTILINE
+}
+
+get '/' do
+  redirect to('/index.html')
+end
+
+post '/match' do
+  params.each{ |k,v| params[k] = CGI::unescape(v)}
   begin
-    match = Regexp.new(opts['regex']).match(opts['test']) 
-    (match ? [true, match.to_s, match.captures] : [false]).to_json
-  rescue RegexpError => ex                                      
-    [false, ex.message].to_json
-  end  
+    opts = params['opts'].split(//).map{ |c| OPTMAP[c] }.reduce{ |x, n| x | n }
+    match = Regexp.new("#{params['regex']}", opts).match(params['test'])
+    if match
+      { :result => true, :match => match.to_s, :captures => match.captures }.to_json
+    else
+      { :result => false}.to_json
+    end
+  rescue RegexpError => ex
+    { :result => false, :error => ex.message}.to_json
+  end
 end
